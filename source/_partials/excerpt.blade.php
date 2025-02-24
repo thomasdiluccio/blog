7e8c923f1b0af285de03850a8683cd70e5ae2583
Thomas di Luccio <thomas.diluccio@platform.sh>
2025-02-24 10:28:35 +0100

    Have larger images be eager loaded

--- source/_partials/excerpt.blade.php
+++ source/_partials/excerpt.blade.php
@@ -10,7 +10,7 @@
                         src="{{ $post->thumbnail($thumbnailSize) }}"
                         alt="{{ $page->featuredImageAlt }}"
                         class="aspect-video w-full rounded-2xl bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2]"
-                        loading="lazy"
+                        loading="{{ $thumbnailSize < 500 ? 'lazy' : 'eager' }}"
                     >
                     <div class="absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-900/10"></div>
                     </a>
